4f64a71abc7c0bb0fcbeb0c6fcae856ae861fd91
Pro2Player <michalp2005@gmail.com>
2023-09-10 22:38:47 +0200

    Some Shitty update

--- Program.cs
+++ Program.cs
@@ -1,39 +1,52 @@
-﻿using System.Threading;
-using SwiftExcel;
+﻿using System.Diagnostics;
 
 
 namespace primes_generator
 {
     class Core
-    {   
-        static readonly List<long> primes = new() { 2};
-        static bool  areWeSaving = false;
+    {
+        static readonly List<long> primes = new() { 2 };
+        static bool areWeSaving = false;
         static int perc;
         static long howMany;
+        static bool add = true;
 
-        public static void Main()
+        public static void Main(string[] args)
         {
-            StartMenu();
+            if(args.Length != 3)    
+                StartMenu();
+            else
+            {
+                howMany = int.Parse(args[0]);
+                perc = (int)((double)howMany / (1 / (Convert.ToDouble(args[1]) / 100)));
+                if (args[2] == "y" || args[2] == "Y")
+                    areWeSaving = true;
+                else
+                    areWeSaving = false;
+            }
 
-
+            var clk = new Stopwatch();
 
             long pc = primes.Count;
-
+            clk.Start();
             for (long i = 3; ; i++)
             {
+
                 if (pc >= howMany)
                     break;
 
 
 
-                if (IsPrime(i))
+                if (IsPrimeAsync(i).Result)
                 {
-                    primes.Add(i); 
+                    primes.Add(i);
                     pc++;
 
                     if (pc % perc == 0 || pc == howMany)
                     {
-                        Console.WriteLine($"{pc}\t{(int)((double)pc / (double)howMany * 100)}%");
+                        clk.Stop();
+                        Console.WriteLine($"{pc}\t{(int)((double)pc / (double)howMany * 100)}%\t{clk.ElapsedMilliseconds}ms");
+                        clk.Restart();
                     }
                 }
             }
@@ -42,13 +55,16 @@ namespace primes_generator
                 Save();
         }
 
-        static bool IsDivisible(long num, long prime)
+        static Task IsDivisible(long num, long prime)
         {
+            Thread.Sleep(1);
             if (num % prime == 0)
             {
-                return true;
+                add = false;
+                return Task.CompletedTask;
             }
-            return false;
+
+            return Task.CompletedTask; ;
         }
 
         static void Save()
@@ -60,7 +76,7 @@ namespace primes_generator
                 string s = p.ToString();
                 primesToSave.Add(s);
                 if (index % perc == 0 || index == howMany - 1)
-                    Console.WriteLine($"{p}\t{(int)(((double)index / (double)(howMany-1)) * 100)}%");
+                    Console.WriteLine($"{p}\t{(int)(((double)index / (double)(howMany - 1)) * 100)}%");
             }
             File.WriteAllLines("PrimeNumbersOut.txt", primesToSave);
         }
@@ -114,19 +130,78 @@ namespace primes_generator
 
         }
 
-        static bool IsPrime(long i)
+        static async Task<bool> IsPrimeAsync(long i)
         {
-            foreach (var j in primes)
+
+            List<Task> tasks = new() { };
+            Parallel.ForEach(primes,(j) =>
             {
-                if (IsDivisible(i, j))
+                var task = new Task(() => IsDivisible(i, j));
+                if (task == null)
                 {
-                    return false;
+                    throw new Exception("Bro wtf");
                 }
+                tasks.Add(task);
+            });
+
+            Thread.Sleep(1); 
+
+            await Task.WhenAll(tasks.ToArray());
+
+            if (add)
+            {
+                add = true;
+                return true;
+
             }
-            return true;
+
+            add = true;
+            return false;
         }
     }
 }
+
+/*WOLNIEJSZE
+static async Task<bool> IsPrimeAsync(long i)
+        {
+
+            List<Task> tasks = new();
+            for (var j = 0; j < primes.Count; j++)
+            {
+                tasks.Add(IsDivisible(i, primes[j]));
+            }
+            await Task.WhenAll(tasks.ToArray());
+
+            if (add)
+            {
+                add = true;
+                return true;
+
+            }
+
+            add = true;
+            return false;
+        }
+ */
+
+/*Szybsze
+        {
+
+            foreach (var j in primes)
+            {
+                IsDivisible(i, j);
+            }
+
+            if (add)
+            {
+                add = true;
+                return true;
+            }
+
+            add = true;
+            return false;
+        }
+ */
 
 
 
