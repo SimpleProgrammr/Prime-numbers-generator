ace0eda9c3c956eb4f085e13551d19b92ce8674c
Programmr <michal.pelon@proton.me>
2025-12-20 18:31:58 +0100

    Java Returning from compact file style

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,81 +1,93 @@
-void main(String[] args) throws IOException {
-    boolean toClear = true;
-    var path = Path.of("./knownNumbers.txt");
-    long upperLimit = 0, lesserLimit = Long.MAX_VALUE;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
-    for (int i = 0; i < args.length; i++) {
-        switch (i) {
-            case 0:
-                lesserLimit = Math.max(2, Long.parseLong(args[0]));
-                break;
-            case 1:
-                upperLimit = Math.min(Long.MAX_VALUE, Long.parseLong(args[1]));
-                break;
-            default:
+public class Main {
+    public void main(String[] args) throws IOException {
+        boolean toClear = true;
+        var path = Path.of("./knownNumbers.txt");
+        long upperLimit = 0, lesserLimit = Long.MAX_VALUE;
 
-                switch (args[i]) {
-                    case "--continue":
-                    case "-c":
-                        toClear = false;
-                        break;
+        for (int i = 0; i < args.length; i++) {
+            switch (i) {
+                case 0:
+                    lesserLimit = Math.max(2, Long.parseLong(args[0]));
+                    break;
+                case 1:
+                    upperLimit = Math.min(Long.MAX_VALUE, Long.parseLong(args[1]));
+                    break;
+                default:
 
-                    case "--sourceFile":
-                    case "-sF":
-                        if (toClear)
+                    switch (args[i]) {
+                        case "--continue":
+                        case "-c":
+                            toClear = false;
                             break;
-                        if (i + 1 >= args.length)
+
+                        case "--sourceFile":
+                        case "-sF":
+                            if (toClear)
+                                break;
+                            if (i + 1 >= args.length)
+                                break;
+                            if (Files.exists(Path.of(args[i + 1])))
+                                path = Path.of(args[++i]);
                             break;
-                        if (Files.exists(Path.of(args[i + 1])))
-                            path = Path.of(args[++i]);
-                        break;
 
-                }
+                    }
 
-                break;
+                    break;
+            }
         }
-    }
 
 
-    if (!Files.exists(path)) {
-        Files.createFile(path);
-    }
-    var alreadyKnownNumbers = new ArrayList<Long>();
-            Files.readAllLines(path).stream().mapToLong(Long::parseLong).forEach(alreadyKnownNumbers::add);
-    if (toClear)
-        alreadyKnownNumbers.clear();
-
-    long startTime = System.nanoTime();
-
-    List<Long> newFound = new ArrayList<>();
-    AtomicLong counter = new AtomicLong();
-    counter.set(0);
-    LongStream.range(lesserLimit, upperLimit).parallel().forEachOrdered(num -> {
-        if (alreadyKnownNumbers.contains(num))
-            return;
-        if (!isPrime(num))
-            return;
-        newFound.add(num);
-        long n = counter.getAndIncrement();
-        IO.println(n + ". Found new prime: " + num);
-    });
-    long endtime = System.nanoTime();
-    double duration = ((double) (endtime - startTime)) / 1000000;
-    IO.println("Time of execution: " + duration + "ms");
-
-    newFound.addAll(alreadyKnownNumbers);
-    Collections.sort(newFound);
-    Files.write(path, newFound.stream().map(Object::toString).collect(Collectors.toList()));
-
-}
-
-
-public static boolean isPrime(long num) {
-    long maxPart = Math.round(Math.sqrt(num));
-
-    for (long i = 2; i <= maxPart; i++) {
-        if (num % i == 0) {
-            return false;
+        if (!Files.exists(path)) {
+            Files.createFile(path);
         }
+        var alreadyKnownNumbers = new ArrayList<Long>();
+        Files.readAllLines(path).stream().mapToLong(Long::parseLong).forEach(alreadyKnownNumbers::add);
+        if (toClear)
+            alreadyKnownNumbers.clear();
+
+        long startTime = System.nanoTime();
+
+        List<Long> newFound = new ArrayList<>();
+        AtomicLong counter = new AtomicLong();
+        counter.set(0);
+        LongStream.range(lesserLimit, upperLimit).parallel().forEachOrdered(num -> {
+            if (alreadyKnownNumbers.contains(num))
+                return;
+            if (!isPrime(num))
+                return;
+            newFound.add(num);
+            long n = counter.getAndIncrement();
+            IO.println(n + ". Found new prime: " + num);
+        });
+        long endtime = System.nanoTime();
+        double duration = ((double) (endtime - startTime)) / 1000000;
+        IO.println("Time of execution: " + duration + "ms");
+
+        newFound.addAll(alreadyKnownNumbers);
+        Collections.sort(newFound);
+        Files.write(path, newFound.stream().map(Object::toString).collect(Collectors.toList()));
+
     }
-    return true;
+
+
+    public static boolean isPrime(long num) {
+        long maxPart = Math.round(Math.sqrt(num));
+
+        for (long i = 2; i <= maxPart; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
