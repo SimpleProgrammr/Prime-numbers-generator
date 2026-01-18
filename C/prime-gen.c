03b32e3edd6b67ceac95c57ad7b572e8d4b964f9
Programmr <michal.pelon@proton.me>
2026-01-18 14:50:09 +0100

    C: - buxfix in bubble sort call

--- C/prime-gen.c
+++ C/prime-gen.c
@@ -260,7 +260,7 @@ int main(int argc, char* argv[]) {
     if (argc > 1 && strcmp(argv[1],"q") == 0)
         quickSort(primes, 0, loaded_primes-1);
     else
-        bubble_sort(primes, 0, storage_size-2);
+        bubble_sort(primes, 0, storage_size-1);
     end = clock();
     printf("Elapsed sorting time: %Lf ms\n",( long double )(end-start)/CLOCKS_PER_SEC*1000);
 
